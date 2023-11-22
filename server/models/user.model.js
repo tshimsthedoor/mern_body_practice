871609e6c0e30a7673f1fb5627c4aee6d6c2eac5
tshimsthedoor <tshims79@gmail.com>
2023-11-22 20:06:02 +0200

    We finish with the User schema model

--- server/models/user.model.js
+++ server/models/user.model.js
@@ -13,6 +13,47 @@ const UserSchema = new mongoose.Schema({
     match: [/.+\@.+\..+/, "Please fill a valid email address"],
     required: "Email is required",
   },
+  created: {
+    type: Date,
+    default: Date.now,
+  },
+  updated: Date,
+  hashed_password: {
+    type: String,
+    required: "Password is required",
+  },
+  salt: String,
 });
+
+UserSchema.virtual("password")
+  .set(function (password) {
+    this._password = password;
+    this.salt = this.makeSalt();
+    this.hashed_password = this.encryptPassword(password);
+  })
+  .get(function () {
+    return this._password;
+  });
+
+UserSchema.method = {
+  authenticate: function (plainText) {
+    return this.encryptPassword(plainText) === this.hashed_password;
+  },
+  encryptPassword: function (password) {
+    if (!password) return "";
+    try {
+      return crypto
+        .createHmac("sha1", this.salt)
+        .update(password)
+        .digest("hex");
+    } catch (err) {
+      return "";
+    }
+  },
+  makeSalt: function () {
+    return Math.round(new Date().valueOf() * Math.random()) + "";
+    
+  },
+};
 
 export default mongoose.model("User", UserSchema);
